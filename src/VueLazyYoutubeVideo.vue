0bb070edb12390c32452935a47fdd8113f180218
Paul Morrison <paul.unr@gmail.com>
2019-12-06 16:20:43 -0500

    Removed Sass, not a required dependancy

--- src/VueLazyYoutubeVideo.vue
+++ src/VueLazyYoutubeVideo.vue
@@ -20,8 +20,8 @@
             <path
               class="y-video__button-shape"
               d="M66.5 7.7c-.8-2.9-2.5-5.4-5.4-6.2C55.8.1 34 0 34 0S12.2.1 6.9 1.6c-3 .7-4.6 3.2-5.4 6.1a89.6 89.6 0 0 0 0 32.5c.8 3 2.5 5.5 5.4 6.3C12.2 47.9 34 48 34 48s21.8-.1 27.1-1.6c3-.7 4.6-3.2 5.4-6.1C68 35 68 24 68 24s0-11-1.5-16.3z"
-            ></path>
-            <path class="y-video__button-icon" d="M45 24L27 14v20"></path>
+            />
+            <path class="y-video__button-icon" d="M45 24L27 14v20" />
           </svg>
         </button>
       </template>
@@ -109,70 +109,54 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style>
 .y-video {
-  $block: &;
   background-color: #000;
   cursor: pointer;
-
-  &__inner {
-    position: relative;
-  }
-
-  &__embed,
-  &__media {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    border-width: 0;
-  }
-
-  &__media {
-    &--type {
-      &--img {
-        object-fit: cover;
-      }
-    }
-  }
-
-  &__button {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    border-width: 0;
-    background-color: transparent;
-    width: 68px;
-    height: 48px;
-    padding: 0;
-    cursor: pointer;
-
-    &-shape {
-      fill: #212121;
-      fill-opacity: 0.8;
-    }
-
-    &-icon {
-      fill: #fff;
-    }
-
-    &:focus {
-      outline: 0;
-
-      #{$block}__button-shape {
-        fill: red;
-        fill-opacity: 1;
-      }
-    }
-  }
-
-  &:hover {
-    #{$block}__button-shape {
-      fill: red;
-      fill-opacity: 1;
-    }
-  }
+}
+.y-video:hover .y-video__button-shape {
+  fill: red;
+  fill-opacity: 1;
+}
+.y-video__inner {
+  position: relative;
+}
+.y-video__embed,
+.y-video__media {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-width: 0;
+}
+.y-video__media--type--img {
+  object-fit: cover;
+}
+.y-video__button {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-width: 0;
+  background-color: transparent;
+  width: 68px;
+  height: 48px;
+  padding: 0;
+  cursor: pointer;
+}
+.y-video__button:focus {
+  outline: 0;
+}
+.y-video__button-shape {
+  fill: #212121;
+  fill-opacity: 0.8;
+}
+.y-video__button-icon {
+  fill: #fff;
+}
+.y-video__button:focus .y-video__button-shape {
+  fill: red;
+  fill-opacity: 1;
 }
 </style>
