2a3ec70f446248a6aae12fae38147e217bd38bc7
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2019-12-07 15:11:56 +0200

    style: prettify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,32 @@
-const path = require("path");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: path.resolve(__dirname, './src/index.js'),
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "vue-lazy-youtube-video.js",
-    library: "VueLazyYoutubeVideo",
-    libraryTarget: "umd",
+    path: path.resolve(__dirname, './dist'),
+    filename: 'vue-lazy-youtube-video.js',
+    library: 'VueLazyYoutubeVideo',
+    libraryTarget: 'umd',
     umdNamedDefine: true,
-    globalObject: "typeof self !== 'undefined' ? self : this"
+    globalObject: "typeof self !== 'undefined' ? self : this",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/
+        loader: 'babel-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.vue$/,
-        loader: "vue-loader"
+        loader: 'vue-loader',
       },
       {
         test: /\.scss$/,
-        use: ["vue-style-loader", "css-loader", "sass-loader"]
-      }
-    ]
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
-  plugins: [
-    new VueLoaderPlugin()
-  ]
-};
+  plugins: [new VueLoaderPlugin()],
+}
