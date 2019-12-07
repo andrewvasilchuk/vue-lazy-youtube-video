c733c5f4b7696cbfc2dd8936429f9ec403ff1612
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2019-12-07 19:23:44 +0200

    Added button slot

--- src/VueLazyYoutubeVideo.vue
+++ src/VueLazyYoutubeVideo.vue
@@ -15,17 +15,19 @@
             :alt="alt"
           />
         </picture>
-        <button type="button" class="y-video__button" :aria-label="buttonLabel">
-          <slot name="icon">
-            <svg viewBox="0 0 68 48" version="1.1" width="100%" height="100%">
-              <path
-                class="y-video__button-shape"
-                d="M66.5 7.7c-.8-2.9-2.5-5.4-5.4-6.2C55.8.1 34 0 34 0S12.2.1 6.9 1.6c-3 .7-4.6 3.2-5.4 6.1a89.6 89.6 0 0 0 0 32.5c.8 3 2.5 5.5 5.4 6.3C12.2 47.9 34 48 34 48s21.8-.1 27.1-1.6c3-.7 4.6-3.2 5.4-6.1C68 35 68 24 68 24s0-11-1.5-16.3z"
-              ></path>
-              <path class="y-video__button-icon" d="M45 24L27 14v20"></path>
-            </svg>
-          </slot>
-        </button>
+        <slot name="button">
+          <button type="button" class="y-video__button" :aria-label="buttonLabel">
+            <slot name="icon">
+              <svg viewBox="0 0 68 48" version="1.1" width="100%" height="100%">
+                <path
+                  class="y-video__button-shape"
+                  d="M66.5 7.7c-.8-2.9-2.5-5.4-5.4-6.2C55.8.1 34 0 34 0S12.2.1 6.9 1.6c-3 .7-4.6 3.2-5.4 6.1a89.6 89.6 0 0 0 0 32.5c.8 3 2.5 5.5 5.4 6.3C12.2 47.9 34 48 34 48s21.8-.1 27.1-1.6c3-.7 4.6-3.2 5.4-6.1C68 35 68 24 68 24s0-11-1.5-16.3z"
+                ></path>
+                <path class="y-video__button-icon" d="M45 24L27 14v20"></path>
+              </svg>
+            </slot>
+          </button>
+        </slot>
       </template>
       <iframe
         v-else
