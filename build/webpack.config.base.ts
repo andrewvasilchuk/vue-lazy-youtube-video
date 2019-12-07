9ba322172ed24d1caaedec1e174b44d4d3a1d4cf
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2019-12-07 15:52:29 +0200

    Added demo and optimized webpack configs

---
+++ build/webpack.config.base.ts
@@ -0,0 +1,25 @@
+import webpack from 'webpack'
+import VueLoaderPlugin from 'vue-loader/lib/plugin'
+
+const config: webpack.Configuration = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+}
+
+export default config
---
+++ demo/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <LazyYoutubeVideo url="https://www.youtube.com/watch?v=4JS70KB9GS0" preview-image-size="maxresdefault" />
+</template>
