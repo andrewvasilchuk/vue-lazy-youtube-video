9d93636b6eb744bad8c6b8f7e55f3cb1d8e974f6
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2019-12-31 19:33:55 +0200

    BREAKING CHANGE: drop support for videoLoaded event

--- src/VueLazyYoutubeVideo.vue
+++ src/VueLazyYoutubeVideo.vue
@@ -130,10 +130,6 @@ export default Vue.extend({
   methods: {
     clickHandler() {
       this.clicked = true
-
-      this.$nextTick(() => {
-        this.$emit('videoLoaded')
-      })
     },
     getPaddingBottom() {
       let { aspectRatio } = this
