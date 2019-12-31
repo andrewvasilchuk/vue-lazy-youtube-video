60cfd31886eb48b891e8d15f5a4f98fc02f3cccd
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2019-12-31 18:44:13 +0200

    Added support for `no-cookie`

--- src/VueLazyYoutubeVideo.vue
+++ src/VueLazyYoutubeVideo.vue
@@ -56,7 +56,7 @@ export default Vue.extend({
     src: {
       type: String,
       required: true,
-      validator: value => value.startsWith('https://www.youtube.com/embed/')
+      validator: value => value.startsWith('https://www.youtube.com/embed/') || value.startsWith('https://www.youtube-nocookie.com/embed/')
     },
     alt: {
       type: String,
@@ -106,7 +106,7 @@ export default Vue.extend({
   },
   computed: {
     id(): string {
-      const regExp = /^https:\/\/www\.youtube\.com\/embed\/(.+)$/
+      const regExp = /^https:\/\/www\.youtube(?:-nocookie)?\.com\/embed\/(.+)$/
       const executionResult = regExp.exec(this.src)
       if (executionResult !== null) {
         return executionResult[1]
