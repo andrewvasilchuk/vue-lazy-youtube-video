c25db494028c445d1a5278ef78bcac17231682c6
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2020-01-01 12:31:08 +0200

    chore(demo): update demo

--- demo/App.vue
+++ demo/App.vue
@@ -1,5 +1,35 @@
 <template>
-  <LazyYoutubeVideo src="https://www.youtube.com/embed/4JS70KB9GS0" />
+  <section>
+    <ul>
+      <li>
+        <LazyYoutubeVideo
+          src="https://www.youtube.com/embed/4JS70KB9GS0"
+          alt="foo"
+          button-label="baz"
+        />
+      </li>
+      <li>
+        <LazyYoutubeVideo
+          src="https://www.youtube.com/embed/4JS70KB9GS0"
+          aspect-ratio="1:1"
+          preview-image-size="default"
+        />
+      </li>
+      <li>
+        <LazyYoutubeVideo
+          src="https://www.youtube.com/embed/4JS70KB9GS0"
+          :thumbnail="{ webp: 'foo', jpg: 'bar' }"
+        />
+      </li>
+      <li>
+        <LazyYoutubeVideo
+          src="https://www.youtube.com/embed/4JS70KB9GS0?start=8"
+          :webp="false"
+          autoplay
+        />
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script lang="ts">
@@ -9,3 +39,16 @@ export default Vue.extend({
   name: 'AppPage',
 })
 </script>
+
+<style>
+ul {
+  padding-left: 0;
+  margin-top: 0;
+  margin-bottom: 0;
+  list-style: none;
+}
+
+li + li {
+  margin-top: 8px;
+}
+</style>
