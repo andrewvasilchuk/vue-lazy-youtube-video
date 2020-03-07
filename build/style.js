6e5d85aa61a466eaac9118bcd4b4d7bdbc7cc216
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2020-03-07 11:39:19 +0200

    build: add separate styles file with no CSS variables

--- build/style.js
+++ build/style.js
@@ -1,7 +1,24 @@
+const fs = require('fs')
 const path = require('path')
 const cpFile = require('cp-file')
+const postcss = require('postcss')
+const postcssCustomProperties = require('postcss-custom-properties')
 
 cpFile(
   path.join(__dirname, '../src/styles/index.css'),
   path.join(__dirname, '../dist/style.css')
 )
+
+postcss([
+  postcssCustomProperties({
+    preserve: false,
+  }),
+])
+  .process(fs.readFileSync(path.resolve(__dirname, '../src/styles/index.css')))
+  .then(result => {
+    fs.writeFile(
+      path.resolve(__dirname, '../dist/style.simplified.css'),
+      result.css,
+      () => true
+    )
+  })
