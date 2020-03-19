0672fb919710feb89e71c9fcca845c96bf294371
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2020-03-19 18:05:05 +0200

    demo: showcase all props

--- demo/App.vue
+++ demo/App.vue
@@ -1,9 +1,35 @@
 <template>
-  <LazyYoutubeVideo
-    src="https://www.youtube.com/embed/4JS70KB9GS0"
-    preview-image-size="maxresdefault"
-    :thumbnail-listeners="thumbnailListeners"
-  />
+  <ul>
+    <li>
+      <LazyYoutubeVideo
+        src="https://www.youtube.com/embed/4JS70KB9GS0"
+        preview-image-size="maxresdefault"
+        alt="foo"
+        button-label="baz"
+        :img-listeners="thumbnailListeners"
+      />
+    </li>
+    <li>
+      <LazyYoutubeVideo
+        src="https://www.youtube.com/embed/4JS70KB9GS0"
+        aspect-ratio="1:1"
+        preview-image-size="default"
+      />
+    </li>
+    <li>
+      <LazyYoutubeVideo
+        src="https://www.youtube.com/embed/4JS70KB9GS0"
+        :thumbnail="{ webp: 'foo', jpg: 'bar' }"
+      />
+    </li>
+    <li>
+      <LazyYoutubeVideo
+        src="https://www.youtube.com/embed/4JS70KB9GS0?start=8"
+        :webp="false"
+        autoplay
+      />
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
