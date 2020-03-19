efee6bbfa3096700d0605777aa791a723f407e89
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2020-03-19 17:56:35 +0200

    build: add minified files of the styles

--- build/style.js
+++ build/style.js
@@ -2,23 +2,53 @@ const fs = require('fs')
 const path = require('path')
 const cpFile = require('cp-file')
 const postcss = require('postcss')
+const cssnano = require('cssnano')
 const postcssCustomProperties = require('postcss-custom-properties')
 
-cpFile(
-  path.join(__dirname, '../src/styles/index.css'),
-  path.join(__dirname, '../dist/style.css')
+const PATH_TO_FILE = path.join(__dirname, '../src/styles/index.css')
+const PATH_TO_MINIFIED_FILE = path.join(__dirname, '../dist/style.min.css')
+const PATH_TO_SIMPLIFIED_FILE = path.join(
+  __dirname,
+  '../dist/style.simplified.css'
+)
+const PATH_TO_SIMPLIFIED_MINIFIED_FILE = path.join(
+  __dirname,
+  '../dist/style.simplified.min.css'
 )
 
-postcss([
-  postcssCustomProperties({
-    preserve: false,
-  }),
-])
-  .process(fs.readFileSync(path.resolve(__dirname, '../src/styles/index.css')))
-  .then(result => {
-    fs.writeFile(
-      path.resolve(__dirname, '../dist/style.simplified.css'),
-      result.css,
-      () => true
-    )
-  })
+cpFile(PATH_TO_FILE, path.join(__dirname, '../dist/style.css')).then(() => {
+  postcss([cssnano()])
+    .process(fs.readFileSync(PATH_TO_FILE), {
+      from: PATH_TO_FILE,
+      to: PATH_TO_MINIFIED_FILE,
+    })
+    .then(result => {
+      fs.writeFile(PATH_TO_MINIFIED_FILE, result.css, () => true)
+    })
+
+  postcss([
+    postcssCustomProperties({
+      preserve: false,
+    }),
+  ])
+    .process(fs.readFileSync(PATH_TO_FILE), {
+      from: PATH_TO_FILE,
+      to: PATH_TO_SIMPLIFIED_FILE,
+    })
+    .then(result => {
+      fs.writeFile(PATH_TO_SIMPLIFIED_FILE, result.css, () => {
+        postcss([cssnano()])
+          .process(fs.readFileSync(PATH_TO_SIMPLIFIED_FILE), {
+            from: PATH_TO_SIMPLIFIED_FILE,
+            to: PATH_TO_SIMPLIFIED_MINIFIED_FILE,
+          })
+          .then(result => {
+            fs.writeFile(
+              PATH_TO_SIMPLIFIED_MINIFIED_FILE,
+              result.css,
+              () => true
+            )
+          })
+      })
+    })
+})
