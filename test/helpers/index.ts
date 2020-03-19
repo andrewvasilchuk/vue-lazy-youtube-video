8b3d4722a043470ab873a5fd862c2832994125fa
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2020-03-19 12:52:37 +0200

    test: enhance types

--- test/helpers/index.ts
+++ test/helpers/index.ts
@@ -1,13 +1,17 @@
 import { Wrapper } from '@vue/test-utils'
 import Component from '../../src/VueLazyYoutubeVideo'
 
-export async function clickAndGetIframe(wrapper: Wrapper<Component>) {
+export async function clickAndGetIframe(
+  wrapper: Wrapper<InstanceType<typeof Component>>
+) {
   wrapper.trigger('click')
   await wrapper.vm.$nextTick()
   return wrapper.find('iframe')
 }
 
-export function getImgAndSourceElements(wrapper: Wrapper<Component>) {
+export function getImgAndSourceElements(
+  wrapper: Wrapper<InstanceType<typeof Component>>
+) {
   const img = wrapper.find('img').element
   const source = wrapper.find('source').element
 
