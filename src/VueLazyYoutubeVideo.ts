8b03072ef803688c18aad39910af6501f5390481
phantomleak <phantomleak@LAPTOP-KOB0IF15.localdomain>
2022-09-27 19:07:02 -0400

    Add functions / conditions to render loom videos

--- src/VueLazyYoutubeVideo.ts
+++ src/VueLazyYoutubeVideo.ts
@@ -13,6 +13,7 @@ import {
   DEFAULT_ASPECT_RATIO,
   DEFAULT_IFRAME_ATTRIBUTES,
   YOUTUBE_REGEX,
+  LOOM_REGEX,
   PLAYER_SCRIPT_SRC,
   PLAYER_CHECK_MS,
 } from './constants'
@@ -26,7 +27,8 @@ export default (Vue as WithRefs<Refs, WithEvents<Events>>).extend({
       required: true,
       validator: (value) =>
         startsWith(value, 'https://www.youtube.com/embed/') ||
-        startsWith(value, 'https://www.youtube-nocookie.com/embed/'),
+        startsWith(value, 'https://www.youtube-nocookie.com/embed/') ||
+        startsWith(value, 'https://www.loom.com/embed/'),
     },
     alt: {
       type: String,
@@ -90,7 +92,13 @@ export default (Vue as WithRefs<Refs, WithEvents<Events>>).extend({
   },
   computed: {
     id(): string {
-      const executionResult = YOUTUBE_REGEX.exec(this.src)
+      var executionResult = null
+      if (startsWith(this.src, 'https://www.loom.com/embed/')){
+        executionResult = LOOM_REGEX.exec(this.src)
+      }
+      else{
+        executionResult = YOUTUBE_REGEX.exec(this.src)
+      }
       if (executionResult !== null) {
         return executionResult[1]
       } else {
@@ -209,7 +217,7 @@ export default (Vue as WithRefs<Refs, WithEvents<Events>>).extend({
         on: { load: this.onIframeLoad },
       })
     },
-    renderThumbnail(h: CreateElement) {
+    renderYoutubeThumbnail(h: CreateElement) {
       return h('picture', {}, [
         this.webp
           ? h('source', {
@@ -227,6 +235,20 @@ export default (Vue as WithRefs<Refs, WithEvents<Events>>).extend({
             src:
               (this.thumbnail && this.thumbnail.jpg) ||
               `https://i.ytimg.com/vi/${this.id}/${this.previewImageSize}.jpg`,
+            alt: this.alt,
+          },
+          on: this.thumbnailListeners,
+        }),
+      ])
+    },
+    renderLoomThumbnail(h: CreateElement){
+      return h('picture', {}, [
+        h('img', {
+          staticClass: 'y-video__media y-video__media--type--img',
+          attrs: {
+            src:
+              (this.thumbnail && this.thumbnail.jpg) ||
+              `https://cdn.loom.com/sessions/thumbnails/${this.id}-00001.jpg`,
             alt: this.alt,
           },
           on: this.thumbnailListeners,
@@ -271,7 +293,8 @@ export default (Vue as WithRefs<Refs, WithEvents<Events>>).extend({
           this.activated
             ? this.renderIframe(h)
             : [
-                this.renderThumbnail(h),
+                startsWith(this.src, 'https://www.loom.com/embed/') ? this.renderLoomThumbnail(h) : this.renderYoutubeThumbnail(h),
+                // this.renderThumbnail(h),
                 this.$slots.button ||
                   h(
                     'button',
